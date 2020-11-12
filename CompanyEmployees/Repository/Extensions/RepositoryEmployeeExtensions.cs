98eb815b976bb391feaa5d42756969623ec2a6ec
phongtt <phongtt.international@gmail.com>
2020-11-13 01:06:05 +0700

    Sorting for employee

--- CompanyEmployees/Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ CompanyEmployees/Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,8 +1,11 @@
 ﻿using Entities.Models;
+using Repository.Extensions.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Linq.Dynamic.Core;
 
 namespace Repository.Extensions
 {
@@ -19,6 +22,17 @@ namespace Repository.Extensions
             var lowerCaseTerm = searchTerm.Trim().ToLower();
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
         }
+        //public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        //{
+        //    if (string.IsNullOrWhiteSpace(orderByQueryString))
+        //        return employees.OrderBy(e => e.Name);
 
+        //    var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+        //    if (string.IsNullOrWhiteSpace(orderQuery))
+        //        return employees.OrderBy(e => e.Name);
+
+        //    return employees.OrderBy(orderQuery);
+        //}
     }
 }
